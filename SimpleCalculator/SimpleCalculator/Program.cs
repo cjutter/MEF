58a97b248091f5113677391e2ab850a37229c142
Charles Utter <cutter@utilant.com>
2016-12-06 07:44:12 -0500

    add calc

---
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+
+        private CompositionContainer _container;
+
+        [Import(typeof(ICalculator))] public ICalculator calculator;
+
+
+
+        public Program()
+        {
+            // An aggregate catalog that combines multiple catalogs
+            var catalog = new AggregateCatalog();
+            //Adds all the parts found in the same assembly as the Program class
+            catalog.Catalogs.Add(new AssemblyCatalog(typeof(Program).Assembly));
+            catalog.Catalogs.Add(new DirectoryCatalog(@"C:\Users\cutter\Documents\MyCode\SimpleCalculator\SimpleCalculator\Extensions"));
+
+            //Create the CompositionContainer with the parts in the catalog
+            _container = new CompositionContainer(catalog);
+
+            //Fill the imports of this object
+            try
+            {
+                this._container.ComposeParts(this);
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+
+            Program p = new Program(); //Composition is performed in the constructor
+            String s;
+            Console.WriteLine("Enter Command:");
+            while (true)
+            {
+                s = Console.ReadLine();
+                Console.WriteLine(p.calculator.Calculate(s));
+            }
+
+        }
+    }
+
+    public interface ICalculator
+    {
+        String Calculate(String input);
+    }
+
+    [Export(typeof(ICalculator))]
+    class MySimpleCalculator : ICalculator
+    {
+        //Lazy<T, TMetadata> is a type provided by MEF to hold indirect references to exports. 
+        [ImportMany]
+        IEnumerable<Lazy<IOperation, IOperationData>> operations;
+
+        public string Calculate(string input)
+        {
+            int left;
+            int right;
+            Char operation;
+            int fn = FindFirstNonDigit(input); //finds the operator
+            if (fn < 0) return "Could not parse command.";
+
+            try
+            {
+                //separate out the operands
+                left = int.Parse(input.Substring(0, fn));
+                right = int.Parse(input.Substring(fn + 1));
+            }
+            catch
+            {
+                return "Could not parse command.";
+            }
+
+            operation = input[fn];
+
+            foreach (Lazy<IOperation, IOperationData> i in operations)
+            {
+                if (i.Metadata.Symbol.Equals(operation)) return i.Value.Operate(left, right).ToString();
+            }
+            return "Operation Not Found!";
+        }
+
+        private int FindFirstNonDigit(String s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!(Char.IsDigit(s[i]))) return i;
+            }
+            return -1;
+        }
+    }
+
+    //value i.value.operate
+    public interface IOperation
+    {
+        int Operate(int left, int right);
+    }
+
+    //metadata
+    //i.metadata
+    public interface IOperationData
+    {
+        Char Symbol { get; }
+    }
+
+    [Export(typeof(IOperation))]
+    [ExportMetadata("Symbol", '+')]
+    class Add : IOperation
+    {
+        public int Operate(int left, int right)
+        {
+            return left + right;
+        }
+    }
+
+    [Export(typeof(IOperation))]
+    [ExportMetadata("Symbol", '-')]
+    class Subtract : IOperation
+    {
+        public int Operate(int left, int right)
+        {
+            return left - right;
+        }
+    }
+}
